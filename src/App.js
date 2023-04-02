0e56a84bf17509ff4b8c05288f985537e295c664
Dexter Vorbe <dextervorbe@ymail.com>
2023-04-01 20:34:53 -0400

    Setting up clarifai api
    Setting up clarifai api auth for face recognition site

--- src/App.js
+++ src/App.js
@@ -4,19 +4,93 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
-import ParticlesBg from 'particles-bg'
+import ParticlesBg from "particles-bg";
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg num={20} type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+const returnCarifaiRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = "cb5edaf03e404443904849e20993f4c8";
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = "dextervorbe";
+  const APP_ID = "facerecognitionbrain";
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = "face-detection";
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+  ///////////////////////////////////////////////////////////////////////////////////////////////////
+  // In this section, we set the user authentication, user and app ID, model details, and the URL
+  // of the image we want as an input. Change these strings to run your own example.
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+    // app.model.predict("face-detection", this.state.input);
+    fetch(
+      "https://api.clarifai.com/v2/models/" +
+        "face-detection" +
+        "/versions/" +
+        "/outputs",
+      returnCarifaiRequestOptions(this.state.input)
+    )
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg num={20} type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
